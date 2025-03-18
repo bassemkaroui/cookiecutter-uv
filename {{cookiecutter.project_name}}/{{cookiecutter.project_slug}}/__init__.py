23840b386878283924b86746d7cfe3ec8c85d17a
Bassem Karoui <bassem.karoui1@gmail.com>
2025-03-18 22:19:27 +0100

    fixed a bug

---
+++ {{cookiecutter.project_name}}/{{cookiecutter.project_slug}}/__init__.py
@@ -0,0 +1,9 @@
+import sys
+
+try:
+    from ._version import version
+except ImportError:
+    print(f"{__name__} version is unknown (package is not installed)")
+    sys.exit(1)
+
+__version__ = version
